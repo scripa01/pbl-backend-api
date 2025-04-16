0bf2509ed9e44d89b9c33d6a70969aa268a586d7
Alexandr Scripcenco <alexclio@mail.ru>
2025-04-16 19:19:56 +0300

    first commit

---
+++ src/test/java/md/pbl/project/pblbackendapi/PblBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package md.pbl.project.pblbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PblBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
